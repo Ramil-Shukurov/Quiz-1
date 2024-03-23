95ed89493743e1158c837f569486bbe9f0ae542b
Ramil_Shukurov <116194891+Ramil-Shukurov@users.noreply.github.com>
2024-03-24 01:57:32 +0400

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,8 +1,22 @@
+
+WEEKS_IN_MONTH = 4.345
+
+# Define the function "calculate_savings" here.
+
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
-  pass
+  print("Choose an action:")
+  print("1.Save more money.")
+  print("2.End.")
+  choice = int(input())
+  while choice != 2:
+    print()
+    calculate_savings()
+    print()
+    print("Choose an action:")
+    print("1.Save more money.")
+    print("2.End.")
+    choice = int(input())
+  print("\nProgram ends.")
 
 if __name__ == "__main__":
   main()
